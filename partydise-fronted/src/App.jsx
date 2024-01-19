a142e87ede7f088a3a42351d5ae6bb52f092f994
snsilvam <silva.mendieta.nicolas@gmail.com>
2024-01-19 17:23:50 -0500

    merge com la rama theme

--- partydise-fronted/src/App.jsx
+++ partydise-fronted/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/login" element={<Login/>} />
-      <Route path="/nuevo trabajador" element={<WorkerCreator/>} />
+      <Route path="/nuevo-trabajador" element={<WorkerCreator/>} />
     </Routes>
   );
 }
